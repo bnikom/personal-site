fbed568ecaf2f7c5d5d51481db201a675fb032bf
Bella Nikom <bnikom@invaluable.com>
2020-09-15 23:10:16 -0400

    resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
     <main className="resume-container">
       <Row className="justify-content-center d-flex mb-4">
         <Col xs={10}>
-          <img className="w-100" src="images/resume-1.png" alt="Bella Nikom's Resume" />
+          <img className="w-100" src="images/resume.png" alt="Bella Nikom's Resume" />
          </Col>
       </Row>
     </main>
